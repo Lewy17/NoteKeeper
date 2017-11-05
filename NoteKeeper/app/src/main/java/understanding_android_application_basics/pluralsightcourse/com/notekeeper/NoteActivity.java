6be89b884e3ebf8c57cba6a6b857f3d3fcb93b47
romansloan <roman.a.sloan@gmail.com>
2017-11-05 01:30:06 -0400

    Modified default layouts. Added a Constraint layout.

--- NoteKeeper/app/src/main/java/understanding_android_application_basics/pluralsightcourse/com/notekeeper/NoteActivity.java
+++ NoteKeeper/app/src/main/java/understanding_android_application_basics/pluralsightcourse/com/notekeeper/NoteActivity.java
@@ -1,15 +1,18 @@
 package understanding_android_application_basics.pluralsightcourse.com.notekeeper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
+
+    Spinner spinnerCourses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,12 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo>adapterCourses = new ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
     }
 
     @Override
